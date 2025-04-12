b8f2c93690dacb39e790913d0c96b1e511f039c4
Anupam Kushwaha <anupamkush9@gmail.com>
2025-04-12 17:14:09 +0530

    importing and exporting component done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login, {Profile, Settings} from './UserComponent'
+//  Login is the default export and Profile and Settings are named export.
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Hello World </h1>
+      <Login></Login>
+      <Profile></Profile>
+      <Settings></Settings>
     </>
   )
 }
