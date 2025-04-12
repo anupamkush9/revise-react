df4bbfc3d8d1cabed5a311bd5f69728ca8edf663
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-12 12:12:29 +0530

    hello world in react

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    {/* here we are rendering App component and componenet 1st letter must be in capital case to identify it differently from html tag. */}
+  </StrictMode>,
+)
