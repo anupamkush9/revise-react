62c3722bda6dc064f668ecd81d0acc19b690c479
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-13 18:34:22 +0530

    select vehicle you have via checkbox done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [input_val, setInput_val] = useState("")
+  const [vehicles, setVehicle] = useState([]);
+
+  const handleVehicles = (event) =>{
+    if(event.target.checked){
+      setVehicle([...vehicles, event.target.value])
+    }else{
+      setVehicle([...vehicles.filter((item) => item != event.target.value)])
+    }
+  }
+
   return (
-    <>
-      <h2> Input value is : {input_val} </h2>
-      <input val={input_val} onChange={(event)=>{setInput_val(event.target.value)}}></input>
-      <h1>Hello World </h1>
-    </>
-  )
+            <>
+              <h1>Hello World </h1>
+
+              <input type="checkbox" onChange={handleVehicles} id="bike" name="bike" value="Bike"/>
+              <label htmlFor="bike"> I have a bike</label><br/>
+
+              <input type="checkbox" onChange={handleVehicles} id="car" name="car" value="Car"/>
+              <label htmlFor="car"> I have a car</label><br/>
+
+              <input type="checkbox" onChange={handleVehicles} id="boat" name="boat" value="Boat"/>
+              <label htmlFor="boat"> I have a boat</label><br/><br/>
+
+              <h2>Selected values are {vehicles.toString()}</h2>
+            </>
+         )
 }
 
 export default App
