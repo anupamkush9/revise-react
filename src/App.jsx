2a08fced0362f44e6048d642f08e8bc38f90faa1
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-15 22:48:10 +0530

    created 2 buttons for toggling the theme and 1 button for handling display grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,107 @@
-function App(){
-  return (
-    <div>
-      <h2 style={{color:"red"}}>Inline Style in React JS</h2>
-      <div style={{
-            border: "5px solid green",
-            width: "200px",
-            boxShadow: "5px 5px 5px 5px grey",
-            padding: "5px",
-      }}>
-      <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" />
-      <div style={{paddingLeft: '10px'}}>
-        <h5> Anupam </h5>
-        <p> Software Engineer</p>
-      </div>
-      </div>
 
-    </div>
+import { useState } from "react";
+
+function App() {
+
+  const [textColour, setTextColour] = useState('white')
+  const [cardStyle, setCardStyle] = useState(
+                                              {
+                                                border: "10px solid #cccccc3b",
+                                                width: "200px",
+                                                boxShadow: "1px 2px 3px 0px #cccccc57",
+                                                margin: "10px"
+                                              }
+                                            )
+  const [grid, setGrid] = useState(true)                             
+
+  const handletheme = (colour, bg_colour)=>{
+    setTextColour(colour)
+    setCardStyle({ ...cardStyle, backgroundColor: bg_colour })
+  }
+
+  return (
+    <>
+      <h2>Hello World</h2>
+      <button onClick={()=>handletheme("white", "black")}> black & white </button>
+      <button onClick={()=>handletheme("blue", "yellow")}> blue & yellow </button>
+      <button onClick={() => setGrid(!grid)} >Toggle Grid</button>
+
+      <div style={{ display : grid ? "flex" :"block", flexWrap: 'wrap'}}>
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+        <div style={cardStyle} >
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <div style={{ color: textColour }}>
+            <h4>Anil Sidhu</h4>
+            <p>Software developer</p>
+          </div>
+        </div> 
+
+      </div>
+    </>
   )
 }
 
-export default App;
+export default App
