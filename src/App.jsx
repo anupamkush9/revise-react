b037fb31930eb8d2449569665abdcacf7804e859
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-16 10:38:05 +0530

    Added css on card by using module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,73 @@
-import './css/style.css'
+// import './css/style.css'
+import style from './css/UserProfile.module.css'
+
 function App() {
 
 
   return (
     <>
-    <h1 className="heading">External Style</h1>
-    <div className='container'>
-    <div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
+    <h1 className={style.heading}>External Style</h1>
+    <div className={style.wrapContainer}>
+      <div className={style.card}>
+        <div>
+          <img className={style.img} src='https://www.w3schools.com/howto/img_avatar.png' />
+        </div>
+        <div className={style.textWrap}>
+          <h4>Anupam Kushwaha</h4>
+          <p>Software developer</p>
+        </div>
       </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div>
-    <div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div>
-    <div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div><div className='user-card'>
-      <div>
-        <img className='img-style' src='https://www.w3schools.com/howto/img_avatar.png' />
-      </div>
-      <div className='text-wrap'>
-        <h4>Anupam Kushwaha</h4>
-        <p>Software developer</p>
-      </div>
-    </div>
 
+      <div className={style.card}>
+        <div>
+          <img className={style.img} src='https://www.w3schools.com/howto/img_avatar.png' />
+        </div>
+        <div className={style.textWrap}>
+          <h4>Anupam Kushwaha</h4>
+          <p>Software developer</p>
+        </div>
+      </div>
+
+      <div className={style.card}>
+        <div>
+          <img className={style.img} src='https://www.w3schools.com/howto/img_avatar.png' />
+        </div>
+        <div className={style.textWrap}>
+          <h4>Anupam Kushwaha</h4>
+          <p>Software developer</p>
+        </div>
+      </div>
+
+      <div className={style.card}>
+        <div>
+          <img className={style.img} src='https://www.w3schools.com/howto/img_avatar.png' />
+        </div>
+        <div className={style.textWrap}>
+          <h4>Anupam Kushwaha</h4>
+          <p>Software developer</p>
+        </div>
+      </div>
+
+
+      <div className={style.card}>
+        <div>
+          <img className={style.img} src='https://www.w3schools.com/howto/img_avatar.png' />
+        </div>
+        <div className={style.textWrap}>
+          <h4>Anupam Kushwaha</h4>
+          <p>Software developer</p>
+        </div>
+      </div>
+
+      <div className={style.card}>
+        <div>
+          <img className={style.img} src='https://www.w3schools.com/howto/img_avatar.png' />
+        </div>
+        <div className={style.textWrap}>
+          <h4>Anupam Kushwaha</h4>
+          <p>Software developer</p>
+        </div>
+      </div>
     </div>
     </>
   )
