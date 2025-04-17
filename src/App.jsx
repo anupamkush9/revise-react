33ecdf8dbf65124f056f0490ac01467415331a7f
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-17 23:52:35 +0530

    created 2 forms with uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
 import { useRef } from "react"
 function App() {
 
-  const inputRef = useRef(null);
-  const h1Ref = useRef(null);
-  
-  const handlefocus = () => {
-    inputRef.current.focus()
-    inputRef.current.placeholder = "focus came"
-    inputRef.current.style.color = "red"
-    inputRef.current.value = "1233"
+  const nameuseref = useRef("")
+  const passworduseref = useRef("")
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+    console.log("name.....password.......",name, password)
   }
 
-  const handleh1click = () => {
-    if (h1Ref.current.style.color != "blue")
-    {
-      h1Ref.current.style.color = "blue"
-    }else{
-      h1Ref.current.style.color = "black"
-    }
+  const handleLoginByUseRef = (event) => {
+    event.preventDefault()
+    const name = nameuseref.current.value;
+    const password = passworduseref.current.value;
+    console.log("name.....password.......",name, password)
   }
+
 
   return (
     <>
-    <h1>Useref in React </h1>
-    <input ref={inputRef} type="text"></input>
-    <button onClick={handlefocus}>focus on input fields</button>
+    <h1> Uncontrolled Component By DOM</h1>
+    <form action="" method="post" onSubmit={handleLogin}>
+      <input type="text" id="name" placeholder="Enter Name"></input>
+      <input type="password" id="password" placeholder="Enter Password"></input>
+      <button>submit</button>
+    </form>
 
-    <h1 ref={h1Ref}>H1 Heading.........</h1>
-    <button onClick={handleh1click}>Toggle h1 colour</button>
+    <hr/>
+    <hr/>
+
+    <h1> Uncontrolled Component By useRef </h1>
+    <form action="" method="post" onSubmit={handleLoginByUseRef}>
+      <input ref={nameuseref} type="text" id="name" placeholder="Enter Name"></input>
+      <input ref={passworduseref} type="password" id="password" placeholder="Enter Password"></input>
+      <button>submit</button>
+    </form>
     </>
   )
 }
