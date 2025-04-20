297548d115e5a470ea4cfb5fabf9289f2041f748
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-20 14:33:18 +0530

    derived-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import {useTransition} from 'react'
+import { useState } from "react";
 
 function App() {
-  const [pending, startTransition] = useTransition()
-  
-  const handleSubmit = ()=>{
-    console.log("handling submit..................")
-      startTransition(async ()=>{
-        await new Promise((res)=>setTimeout( res, 2000))
-      })
+  const [users,setUsers]=useState([]);
+  const [user,setUser]=useState('');
+  const handleAddUsers=()=>{
+    setUsers([...users,user])
   }
+const total=users.length;
+const last= users[users.length-1];
+const unique= [...new Set(users)].length
 
 
-  // new Promise(res => setTimeout(res, 5000));
-
-  // This is just shorthand for:
-
-  // new Promise((resolve, reject) => {
-  //   setTimeout(resolve, 5000);
-  // });
-  
   return (
-   <div>
-    <h1>useTransition Hook in React js 19</h1>
-    <button disabled={pending} onClick={handleSubmit}>Submit</button>
-   </div>
+    <div>
+      <h2>Total User : {total} </h2>
+      <h2>Last User : {last}</h2>
+      <h2>Unique Total User : {unique}</h2>
+
+    <input type="text" onChange={(event)=>setUser(event.target.value)} placeholder="add new user" />
+    <button onClick={handleAddUsers} >Add User</button>
+    {
+      users.map((item,index)=>(
+        <h4 key={index}>{item}</h4>
+      ))
+    }
+    </div>
 
   );
 }
