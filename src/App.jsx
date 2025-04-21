a2ffb4c554f8b5e2c0cd7845e5e7450de8491773
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-22 04:52:14 +0530

    rendered same form multiple time with useId hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-import { useActionState, useState } from "react";
+import { useId } from "react";
 
 function App() {
 
-  const [userNames, setuserNames] = useState(["Virat", "hardik"]);
-  const handleLastName = (event) =>{
-    userNames[userNames.length-1] = event.target.value
-    setuserNames([...userNames])
-  }
-
-  const [dataDetails,setDataDetails]=useState([
-    { name:'anil',age:'29'},
-    { name:'sam',age:'25'},
-    { name:'peter',age:'33'},
-  ])
-
-  const handleAge = (event)=>{
-    dataDetails[dataDetails.length-1].age = event.target.value
-    setDataDetails([...dataDetails])
-  }
   return (
     <div>
-      <h1>Updating Array of string</h1>
-      <input type="text" placeholder="Enter name to change last name" onChange={handleLastName}/>
-      {
-      userNames.map((user_name, index)=>(<h5 key={index}>{user_name}</h5>))
-      }
-    
-    <hr/>
-    <hr/>
-      <h1>Updating Array of objects</h1>
-      <input type="text" placeholder="Enter Age to change last person age" onChange={handleAge}/>
-      {
-      dataDetails.map((data, index)=>(<h5 key={index}>{data?.name} {data?.age}</h5>))
-      }
-
+      <h3>useId hook</h3>
+      <UserForm/>
+      <UserForm/>
+      <UserForm/>
     </div>
   );
+}
+
+function UserForm(){
+  const id = useId()
+  return (
+    <div>
+      <label htmlFor={id+"name"}> Enter Name : </label>
+      <input id={id+"name"} name="name" type="text" placeholder="Enter name"></input>
+      <br/>
+      <br/>
+      <label htmlFor={id+"password"}> Enter Password : </label>
+      <input id={id+"password"} name="password" type="text" placeholder="Enter password"></input>
+      <br/>
+      <br/>
+      <button>Submit</button>
+      <hr/>
+    </div>
+  )
 }
 
 export default App;
