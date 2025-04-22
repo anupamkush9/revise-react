f25e2e162acc99f87a5bf1ed1baa1c94458f3e89
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-22 22:55:03 +0530

    created dynamically a list of td for a tbody

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useId } from "react";
+
+import { Fragment } from "react";
 
 function App() {
 
   return (
-    <div>
-      <h3>useId hook</h3>
-      <UserForm/>
-      <UserForm/>
-      <UserForm/>
-    </div>
-  );
-}
+    <>
+      <h2>Fragment in React Js</h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Roll No</td>
+            <td>Name</td>
+            <td>marks</td>
+          </tr>
+        </thead>
 
-function UserForm(){
-  const id = useId()
-  return (
-    <div>
-      <label htmlFor={id+"name"}> Enter Name : </label>
-      <input id={id+"name"} name="name" type="text" placeholder="Enter name"></input>
-      <br/>
-      <br/>
-      <label htmlFor={id+"password"}> Enter Password : </label>
-      <input id={id+"password"} name="password" type="text" placeholder="Enter password"></input>
-      <br/>
-      <br/>
-      <button>Submit</button>
-      <hr/>
-    </div>
+        <tbody>
+          <TableBody></TableBody>
+          <TableBodyShort/>
+        </tbody>
+      </table>
+    </>
   )
 }
 
-export default App;
+function TableBody() {
+  const fragment_rows = [
+    [1, 'Anupam', 92],
+    [2, 'Raj', 85],
+    [3, 'Vikram', 78]
+  ];
 
+  return (
+    <Fragment>
+      {fragment_rows.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+          {row.map((cell, cellIndex) => (
+            <td key={cellIndex}>{cell}</td>
+          ))}
+        </tr>
+      ))}
+    </Fragment>
+  );
+}
+
+function TableBodyShort() {
+  const rows = [
+    [4, 'Virat', 88],
+    [5, 'Abhishek', 65],
+    [6, 'Hardik', 78]
+  ];
+
+  return (
+    <>
+      {rows.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+          {row.map((cell, cellIndex) => (
+            <td key={cellIndex}>{cell}</td>
+          ))}
+        </tr>
+      ))}
+    </>
+  );
+}
+
+export default App
