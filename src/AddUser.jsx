da56fd16fbe889350f4c98d439916744b784eaa5
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-28 01:02:14 +0530

    added bootstrap class in add user component

--- src/AddUser.jsx
+++ src/AddUser.jsx
@@ -2,16 +2,14 @@ export default function(){
     return (
         <>
             <div className="container mt-4">
-                <input type="text" placeholder="Enter Name"/>
+                <input className="form-control" type="text" placeholder="Enter Name"></input>
+                <br />
+                <input className="form-control" type="text" placeholder="Enter Age"></input>
                 <br />
                 <br />
-                <input type="text" placeholder="Enter Age"/>
+                <input type="email" className="form-control" placeholder="Enter Email Address"/>
                 <br />
-                <br />
-                <input type="text" placeholder="Enter Email"/>
-                <br />
-                <br />
-                <button>Submit</button>
+                <button type="button" className="btn btn-success btn-lg">Success</button>
             </div>
         </>
     )
