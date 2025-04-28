107ab0f938afd33e2b1b639255ffab36ad82fa7b
Anupamkush9 <anupam.kush9@gmail.com>
2025-04-28 22:34:42 +0530

    implemented fetch post api on click of add user button

--- src/AddUser.jsx
+++ src/AddUser.jsx
@@ -1,16 +1,68 @@
-export default function(){
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+
+export default function () {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+
+    const handleCreateUser = async () => {
+        try {
+            const response = await fetch("http://localhost:8000/users/", {
+                method: "POST", // Important: POST method for creating
+                headers: {
+                    "Content-Type": "application/json", // Telling server we are sending JSON
+                },
+                body: JSON.stringify({ name: name, age: age, email: email }), // Converting JS object to JSON
+            });
+
+            const data = await response.json();
+            console.log("Created User:", data);
+            alert("user Created")
+            navigate("/")
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <>
             <div className="container mt-4">
-                <input className="form-control" type="text" placeholder="Enter Name"></input>
+                <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    type="text"
+                    placeholder="Enter Name"
+                ></input>
                 <br />
-                <input className="form-control" type="text" placeholder="Enter Age"></input>
+                <input
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    className="form-control"
+                    type="text"
+                    placeholder="Enter Age"
+                ></input>
                 <br />
+                <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter Email Address"
+                />
                 <br />
-                <input type="email" className="form-control" placeholder="Enter Email Address"/>
-                <br />
-                <button type="button" className="btn btn-success btn-lg">Success</button>
+                <button
+                    type="button"
+                    onClick={handleCreateUser}
+                    className="btn btn-success btn-lg"
+                >
+                    Add User
+                </button>
             </div>
         </>
-    )
+    );
 }
