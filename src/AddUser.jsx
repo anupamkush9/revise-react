a4d39ceda401cbb6f8d3f69cd25383124c1e2e84
Anupamkush9 <anupam.kush9@gmail.com>
2025-05-01 05:39:52 +0530

    Added validation on click of submit form with the help of useActionState hook

--- src/AddUser.jsx
+++ src/AddUser.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useState, useActionState } from "react";
 import { useNavigate } from "react-router";
 
 
 export default function () {
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
-    const [email, setEmail] = useState("");
     const navigate = useNavigate();
+    
+    const handleCreateUser = async (prevData, formData) => {
+        const name = formData.get("name")
+        const age = parseInt(formData.get("age"), 10)
+        const email = formData.get("email")
 
-    const handleCreateUser = async () => {
+        if (name.length > 10) {
+            return { name: "Name cannot have more than 10 characters." };
+        } else if (age > 60) {
+            return { age: "Age cannot be more than 60." };
+        }
+
         try {
             const response = await fetch("http://localhost:8000/users/", {
                 method: "POST", // Important: POST method for creating
@@ -17,51 +24,59 @@ export default function () {
                 },
                 body: JSON.stringify({ name: name, age: age, email: email }), // Converting JS object to JSON
             });
-
-            const data = await response.json();
-            console.log("Created User:", data);
-            alert("user Created")
-            navigate("/")
+            if (response.ok){
+                navigate("/")
+                return {message:"User Added Successfully."}
+            }else{
+                const data = await response.json();
+                return data
+            }
         } catch (error) {
             console.error("Error fetching user data:", error);
-        } finally {
-            setLoading(false);
+            return {error:"Error in User Creation."}
         }
     };
+    const [data, formAction, isPending] = useActionState(handleCreateUser, {}); // 2nd args is the initial value of data
     return (
         <>
             <div className="container mt-4">
-                <input
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="form-control"
-                    type="text"
-                    placeholder="Enter Name"
-                ></input>
-                <br />
-                <input
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                    className="form-control"
-                    type="text"
-                    placeholder="Enter Age"
-                ></input>
-                <br />
-                <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter Email Address"
-                />
-                <br />
-                <button
-                    type="button"
-                    onClick={handleCreateUser}
-                    className="btn btn-success btn-lg"
-                >
-                    Add User
-                </button>
+                <form action={formAction}>
+                    <input
+                        name="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter Name"
+                    ></input>
+                    {
+                        data.name && <span style={{ color: "red" }}>{data.name}</span>
+                    }
+                    <br />
+                    <input
+                        name="age"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter Age"
+                    ></input>
+                    {
+                        data.age && <span style={{ color: "red" }}>{data.age}</span>
+                    }
+                    <br />
+                    <input
+                        name="email"
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter Email Address"
+                    />
+                    {data?.message && <span style={{ color: "green" }}>{data.message}</span>}
+                    <br />
+                    <button
+                        type="submit"
+                        className="btn btn-success btn-lg"
+                        disabled={isPending}
+                    >
+                        Add User
+                    </button>
+                </form>
             </div>
         </>
     );
