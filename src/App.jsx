363055b05f298f561792297b91c76c7679a3f0bc
Anupamkush9 <anupam.kush9@gmail.com>
2025-05-03 18:12:13 +0530

    created button click counter by using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,22 @@
 
-import { Fragment } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+      document.title = `You clicked ${count} times`;
+  }, [count]);
+
 
   return (
     <>
-      <h2>Fragment in React Js</h2>
-      <table>
-        <thead>
-          <tr>
-            <td>Roll No</td>
-            <td>Name</td>
-            <td>marks</td>
-          </tr>
-        </thead>
-
-        <tbody>
-          <TableBody></TableBody>
-          <TableBodyShort/>
-        </tbody>
-      </table>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Click {count} times{" "}
+      </button>
     </>
   )
-}
-
-function TableBody() {
-  const fragment_rows = [
-    [1, 'Anupam', 92],
-    [2, 'Raj', 85],
-    [3, 'Vikram', 78]
-  ];
-
-  return (
-    <Fragment>
-      {fragment_rows.map((row, rowIndex) => (
-        <tr key={rowIndex}>
-          {row.map((cell, cellIndex) => (
-            <td key={cellIndex}>{cell}</td>
-          ))}
-        </tr>
-      ))}
-    </Fragment>
-  );
-}
-
-function TableBodyShort() {
-  const rows = [
-    [4, 'Virat', 88],
-    [5, 'Abhishek', 65],
-    [6, 'Hardik', 78]
-  ];
-
-  return (
-    <>
-      {rows.map((row, rowIndex) => (
-        <tr key={rowIndex}>
-          {row.map((cell, cellIndex) => (
-            <td key={cellIndex}>{cell}</td>
-          ))}
-        </tr>
-      ))}
-    </>
-  );
 }
 
 export default App
