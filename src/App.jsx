ed795d056cc5d448f6e5bd5f572e9165f0d85fa4
Anupamkush9 <anupam.kush9@gmail.com>
2025-05-06 02:29:48 +0530

    implemented basic lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,24 @@
-import React, { useReducer } from 'react';
+import React from "react";
+import { Suspense, lazy } from "react";
+const Component1 = lazy(() => import("./Component1"));
+const Component2 = lazy(() => import("./Component2"));
 
-// Reducer function
-function counterReducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return { count: state.count + 1 };
-    case 'DECREMENT':
-      return { count: state.count - 1 };
-    case 'RESET':
-      return { count: 0 };
-    case 'SET':
-      return { count: action.payload };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+function App() {
+	return (
+		<>
+			<h1> Lazy Load</h1>
+			<Suspense
+				fallback={<div>Component1 is loading please wait...</div>}
+			>
+				<Component1 />
+			</Suspense>
+			<Suspense
+				fallback={<div>Component2 is loading please wait...</div>}
+			>
+				<Component2 />
+			</Suspense>
+		</>
+	);
 }
 
-function Counter() {
-  // Initialize useReducer
-  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
-
-  return (
-    <div className="container mt-5 text-center">
-      <div className="card p-4 shadow-lg">
-        <h2 className="mb-4">Counter App</h2>
-        <h3 className="display-4 mb-4">Count: {state.count}</h3>
-
-        <div className="btn-group mb-3" role="group">
-          <button className="btn btn-success" onClick={() => dispatch({ type: 'INCREMENT' })}>
-            +
-          </button>
-          <button className="btn btn-danger" onClick={() => dispatch({ type: 'DECREMENT' })}>
-            -
-          </button>
-          <button className="btn btn-secondary" onClick={() => dispatch({ type: 'RESET' })}>
-            Reset
-          </button>
-          <button className="btn btn-primary" onClick={() => dispatch({ type: 'SET', payload: state.count + 10 })}>
-            Increase by 10
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Counter;
+export default App;
---
+++ src/Component1.jsx
@@ -0,0 +1,9 @@
+function Component1() {
+    return ( 
+        <>
+            <h1>Component 1</h1>
+        </>
+     );
+}
+
+export default Component1;
