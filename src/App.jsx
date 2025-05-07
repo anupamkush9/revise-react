6053a78fea928b6b1f68bbe31d73382b30a7b84f
Anupamkush9 <anupam.kush9@gmail.com>
2025-05-07 09:04:15 +0530

    implemente range input to select the value of rgb and displayed it's colour combination on ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,43 @@
 
-import { Fragment } from "react";
+import { useState } from "react";
 
 function App() {
+  const colors=JSON.parse( localStorage.getItem('color'))
+  const [r, setR] = useState(colors && colors.r?colors.r:0);
+  const [g, setG] = useState(colors && colors.g?colors.g:0);
+  const [b, setB] = useState(colors && colors.b?colors.b:0);
+
+  const save = () => {
+    localStorage.setItem("color",JSON.stringify({r,g,b}))
+    alert("Saved Color combination")
+  }
 
   return (
-    <>
-      <h2>Fragment in React Js</h2>
-      <table>
-        <thead>
-          <tr>
-            <td>Roll No</td>
-            <td>Name</td>
-            <td>marks</td>
-          </tr>
-        </thead>
+    <div className="container m-5">
+      <div style={{
+        backgroundColor: `rgb(${r}, ${g}, ${b})`,
+        height: 200,
+        width: 200
+      }}>
 
-        <tbody>
-          <TableBody></TableBody>
-          <TableBodyShort/>
-        </tbody>
-      </table>
-    </>
+      </div>
+      <br/>
+      <br/>
+      <label htmlFor="redRange" className="form-label">Red</label>
+      <input type="range" value={r} onChange={(e)=>(setR(e.target.value))} className="form-range" min="0" max="255" id="redRange"/>
+      <br/>
+      <br/>
+      <label htmlFor="greenRange" className="form-label">Green</label>
+      <input type="range" value={g} onChange={(e)=>(setG(e.target.value))} className="form-range" min="0" max="255" id="greenRange"/>
+      <br/>
+      <br/>
+      <label htmlFor="blueRange" className="form-label">Blue</label>
+      <input type="range" value={b} onChange={(e)=>(setB(e.target.value))} className="form-range" min="0" max="255" id="blueRange"/>
+      <br/>
+      <br/>
+      <button onClick={save} type="button" className="btn btn-success">Save Color Combination</button>
+    </div>
   )
-}
-
-function TableBody() {
-  const fragment_rows = [
-    [1, 'Anupam', 92],
-    [2, 'Raj', 85],
-    [3, 'Vikram', 78]
-  ];
-
-  return (
-    <Fragment>
-      {fragment_rows.map((row, rowIndex) => (
-        <tr key={rowIndex}>
-          {row.map((cell, cellIndex) => (
-            <td key={cellIndex}>{cell}</td>
-          ))}
-        </tr>
-      ))}
-    </Fragment>
-  );
-}
-
-function TableBodyShort() {
-  const rows = [
-    [4, 'Virat', 88],
-    [5, 'Abhishek', 65],
-    [6, 'Hardik', 78]
-  ];
-
-  return (
-    <>
-      {rows.map((row, rowIndex) => (
-        <tr key={rowIndex}>
-          {row.map((cell, cellIndex) => (
-            <td key={cellIndex}>{cell}</td>
-          ))}
-        </tr>
-      ))}
-    </>
-  );
 }
 
 export default App
