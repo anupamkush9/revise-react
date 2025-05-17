d6c4a9e5a94df616bf160a1d34abc39b4367aa99
Anupamkush9 <anupam.kush9@gmail.com>
2025-05-17 19:34:09 +0530

    star rating project by module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,23 @@
 
 import { useState } from "react";
+import style from "./css/rating_star.module.css"
 
 function App() {
-  const colors=JSON.parse( localStorage.getItem('color'))
-  const [r, setR] = useState(colors && colors.r?colors.r:0);
-  const [g, setG] = useState(colors && colors.g?colors.g:0);
-  const [b, setB] = useState(colors && colors.b?colors.b:0);
-
-  const save = () => {
-    localStorage.setItem("color",JSON.stringify({r,g,b}))
-    alert("Saved Color combination")
-  }
-
+  let stars = [0, 0, 0, 0, 0]
+  const [rating, SetRating] = useState(0)
+  const [hover, SetHover] = useState(0)
+  console.log("hover===>>",hover)
   return (
-    <div className="container m-5">
-      <div style={{
-        backgroundColor: `rgb(${r}, ${g}, ${b})`,
-        height: 200,
-        width: 200
-      }}>
-
-      </div>
-      <br/>
-      <br/>
-      <label htmlFor="redRange" className="form-label">Red</label>
-      <input type="range" value={r} onChange={(e)=>(setR(e.target.value))} className="form-range" min="0" max="255" id="redRange"/>
-      <br/>
-      <br/>
-      <label htmlFor="greenRange" className="form-label">Green</label>
-      <input type="range" value={g} onChange={(e)=>(setG(e.target.value))} className="form-range" min="0" max="255" id="greenRange"/>
-      <br/>
-      <br/>
-      <label htmlFor="blueRange" className="form-label">Blue</label>
-      <input type="range" value={b} onChange={(e)=>(setB(e.target.value))} className="form-range" min="0" max="255" id="blueRange"/>
-      <br/>
-      <br/>
-      <button onClick={save} type="button" className="btn btn-success">Save Color Combination</button>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      {
+        stars.map((ele, index) => (
+          <span  className={index < rating || index < hover ? style.star : style.unstar}
+                 key={index} onClick={() => SetRating(index + 1)}
+                 onMouseEnter={()=>SetHover(index+1)}
+                 onMouseLeave={()=>SetHover(index)}>
+                  &#9733;</span>
+        ))
+      }
     </div>
   )
 }
