3b0e48f434fc62ac81fcac9b4ee2ad0e9daa7c8a
Anupamkush9 <anupam.kush9@gmail.com>
2025-06-30 00:10:14 +0530

    add to cart funcationality implemented by redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
 
 function App() {
-  const current_time = new Date().toLocaleTimeString()
-  const [time, SetTime] = useState(current_time)
-
-  setInterval(() => {
-    const time = new Date().toLocaleTimeString()
-    SetTime(time)
-  }, 1000);
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-        <div>
-          <h3 className="text-primary">Digital Clock</h3>
-          <h3>{time}</h3>
-        </div>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
-const now = new Date();
+
+
+// ref https://www.youtube.com/watch?v=HNlMsiFEUuE&t=3608s
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+import store from './Redux/store'
+import { Provider } from 'react-redux'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
     {/* here we are rendering App component and componenet 1st letter must be in capital case to identify it differently from html tag. */}
   </StrictMode>,
 )
